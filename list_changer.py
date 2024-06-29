29f1877c7f5b992eb0ca79cad9d2ec208c9c90eb
MikoChudzin <miko.chudzinski@gmail.com>
2024-06-29 22:55:46 +0200

    Update main with comments

--- list_changer.py
+++ list_changer.py
@@ -4,7 +4,7 @@ with open('D:\\Kodowanie\\Personal\\decklist.txt', 'r') as f2:
     lines_to_modify = f2.readlines()
 with open('D:\\Kodowanie\\Personal\\ver2.txt', 'w') as f3:
     for line1 in lines_to_modify:
-        i = 0
+        i = 0        # deathflag variable
         for line2 in lines_to_remove:
             if line1.strip("\n") == line2.strip("\n"):
                 i += 1        
@@ -12,7 +12,7 @@ with open('D:\\Kodowanie\\Personal\\ver2.txt', 'w') as f3:
             f3.write(line1)
 
 with open('D:\\Kodowanie\\Personal\\ver3.txt', 'w') as f4:
-    lines_of_result = set(lines_to_modify)-set(lines_to_remove)
+    lines_of_result = set(lines_to_modify)-set(lines_to_remove)    # using sets this task becomes way simpler
     result = list(lines_of_result)
     result.sort()
     for line in result:
